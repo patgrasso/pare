c4b1a629a33e2cccc415a7974401b34bc9a64401
patgrasso <pgrasso@stevens.edu>
2016-07-16 21:35:06 -0400

    Separate products into product/listings
    Reflect changes in DB init/clean scripts. Also, set up a quick demo in
    server.js

--- server.js
+++ server.js
@@ -6,8 +6,44 @@
 const express = require('express');
 const app     = express();
 const router  = require('./app/router');
+const database = require('./app/models/database');
 
 app.use('/', router);
+
+
+// FIXME: All of the below is subject to SQL injection
+app.get('/make', (req, res) => {
+  if (req.query.name == null || req.query.price == null) {
+    res.status(400).send('You suck');
+  } else {
+    console.log(req.query);
+    database.query(
+      'insert into products (name, price, quantity, date, category) ' +
+      'values ($1, $2, $3, $4, $5)',
+      [
+        req.query.name,
+        parseFloat(req.query.price),
+        parseInt(req.query.quantity),
+        req.query.date,
+        req.query.category
+      ],
+      (err, result) => {
+        res.status((err) ? 400 : 200).send(err || result);
+      }
+    );
+  }
+});
+
+app.get('/find', (req, res) => {
+  let query = Object.keys(req.query).map((param) => {
+    return param + '=\'' + req.query[param] + '\'';
+  }).join(' and ');
+
+  console.log(query);
+  database.query('select * from products where ' + query, [], (err, result) => {
+    res.status((err) ? 400 : 200).send(err || result.rows);
+  });
+});
 
 app.listen(8000, () => {
   console.log('Starting up on localhost:8000');
