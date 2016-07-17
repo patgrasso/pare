0a6a1d33dbaff2edee34d2c45648ec0ba6dad40f
halfbro <mlagaren@stevens.edu>
2016-07-16 21:41:53 -0400

    Add body-parser to dependencies and use it as middleware

--- app/router.js
+++ app/router.js
@@ -2,8 +2,11 @@
  * Set up each router separately and export for use by the application
  */
 
-const express = require('express');
-let   router  = express.Router();
+const express    = require('express');
+const bodyParser = require('body-parser');
+let   router     = express.Router();
+
+router.use(bodyParser.json());
 
 router.use('/api', require('./routes/api'));
 
